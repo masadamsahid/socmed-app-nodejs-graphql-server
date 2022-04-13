d8b61787f8c66ecd07ac42435f5d1f3b5d862a7e
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-04-13 11:55:13 +0700

    unignore config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb://masadamsahid:supermanadmin@cluster0-shard-00-00.kdjd6.mongodb.net:27017,cluster0-shard-00-01.kdjd6.mongodb.net:27017,cluster0-shard-00-02.kdjd6.mongodb.net:27017/merng?ssl=true&replicaSet=atlas-11bteo-shard-0&authSource=admin&retryWrites=true&w=majority',
+    SECRET_KEY: 'very secret key hehey'
+}
